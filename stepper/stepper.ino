e0c487468875ac0d2a464e405c4785c3cfa7baef
taom02 <jjpabonz@unal.edu.co>
2014-09-15 21:59:20 -0500

    stepper

---
+++ stepper/stepper.ino
@@ -0,0 +1,28 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+byte time;
+if (Serial.available()){
+  time=Serial.read();
+  digitalWrite(8,HIGH);
+  delay(time);
+  digitalWrite(8,LOW);
+    digitalWrite(9,HIGH);
+  delay(time);
+  digitalWrite(9,LOW);
+    digitalWrite(10,HIGH);
+  delay(time);
+  digitalWrite(10,LOW);
+    digitalWrite(11,HIGH);
+  delay(time);
+  digitalWrite(11,LOW);
+}
+}
