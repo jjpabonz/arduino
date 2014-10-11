7b68bbbf8f86a05602e206860ccbb5dd03bf7cd3
taom02 <jjpabonz@unal.edu.co>
2014-10-10 22:30:31 -0500

    step control

---
+++ step_funcional/step_funcional.ino
@@ -0,0 +1,27 @@
+#include <Stepper.h>
+#define Steps 200
+Stepper motor(Steps, 8, 9, 10, 11);
+void setup() {
+  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  motor.setSpeed(10);
+}
+void loop() {
+byte x=0; long y=0;  int z=1;
+         // send data only when you receive data:
+while(Serial.available() > 0) {
+                 // read the incoming byte:
+  x = Serial.read();
+    if(x!=45){
+      y=(y*10)+z*(x-48);
+      z=1;
+    }  
+    else{
+      z=-1;
+      x=0;
+     }
+}        // say what you got:
+ motor.step(y);    
+ Serial.print("I received: ");
+ Serial.println(y, DEC);
+ delay(1000);
+}
