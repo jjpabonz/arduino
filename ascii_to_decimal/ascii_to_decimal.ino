c51a9cf6c15a89bde9f26226b605a255bab633c3
taom02 <jjpabonz@unal.edu.co>
2014-10-10 22:30:06 -0500

    ascii to decimal

---
+++ ascii_to_decimal/ascii_to_decimal.ino
@@ -0,0 +1,32 @@
+   // for incoming serial data
+
+void setup() {
+         Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+byte x=0;
+long y=0;
+int z=1;
+         // send data only when you receive data:
+         while(Serial.available() > 0) {
+                 // read the incoming byte:
+                 
+                 x = Serial.read();
+                    if(x!=45){
+                      y=(y*10)+z*(x-48);
+                      z=1;
+ 
+                  }  
+                    else{
+                      z=-1;
+                      x=0;
+  
+                  }
+
+         }        // say what you got:
+                 
+                 Serial.print("I received: ");
+                 Serial.println(y, DEC);
+         delay(1000);
+}
